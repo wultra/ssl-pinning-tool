0637d90944871440d11980a27e2661bf4470af2f
romanstrobl <roman.strobl@wultra.com>
2018-09-03 22:01:18 +0200

    Update help text

--- src/main/java/com/wultra/security/ssl/pinning/Application.java
+++ src/main/java/com/wultra/security/ssl/pinning/Application.java
@@ -195,7 +195,7 @@ public class Application {
         final Options options = new Options();
         options.addOption("h", "help", false, "Print this help manual.");
         options.addOption("k", "keys", true, "EC key pair in PEM format stored as PKCS #8.");
-        options.addOption("p", "password", true, "Password for encrypted key pair file in PEM format.");
+        options.addOption("p", "password", true, "Password for encrypted key pair file.");
         options.addOption("o", "output", true, "Output file.");
         options.addOption("n", "name", true, "Domain common name.");
         options.addOption("f", "fingerprint", true, "SHA-256 fingerprint of certificate in HEX format.");
