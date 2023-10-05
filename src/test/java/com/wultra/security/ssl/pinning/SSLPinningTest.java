4aff41025d802c1cc71b9c2e0cf6d7fa96453464
Lubos Racansky <lubos.racansky@gmail.com>
2023-10-05 08:07:27 +0200

    Fix #67: Enhance SSLPinningTest
    - Declared SignatureException is not thrown, replace by general Exception everywhere
    - Use text block

--- src/test/java/com/wultra/security/ssl/pinning/SSLPinningTest.java
+++ src/test/java/com/wultra/security/ssl/pinning/SSLPinningTest.java
@@ -3,8 +3,6 @@ package com.wultra.security.ssl.pinning;
 import com.google.common.io.BaseEncoding;
 import com.wultra.security.ssl.pinning.errorhandling.SSLPinningException;
 import com.wultra.security.ssl.pinning.model.CertificateInfo;
-import io.getlime.security.powerauth.crypto.lib.model.exception.CryptoProviderException;
-import io.getlime.security.powerauth.crypto.lib.model.exception.GenericCryptoException;
 import io.getlime.security.powerauth.crypto.lib.util.SignatureUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.security.*;
+import java.security.PublicKey;
+import java.security.Security;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,7 +25,7 @@ class SSLPinningTest {
     private static final String TEST_CERTIFICATE_BASE64 = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUR4ekNDQXErZ0F3SUJBZ0lJZkd4NURBK3VZS1F3RFFZSktvWklodmNOQVFFTEJRQXdWREVMTUFrR0ExVUUKQmhNQ1ZWTXhIakFjQmdOVkJBb1RGVWR2YjJkc1pTQlVjblZ6ZENCVFpYSjJhV05sY3pFbE1DTUdBMVVFQXhNYwpSMjl2WjJ4bElFbHVkR1Z5Ym1WMElFRjFkR2h2Y21sMGVTQkhNekFlRncweE9EQTRNVFF3TnpRME16VmFGdzB4Ck9ERXdNak13TnpNNE1EQmFNR2d4Q3pBSkJnTlZCQVlUQWxWVE1STXdFUVlEVlFRSURBcERZV3hwWm05eWJtbGgKTVJZd0ZBWURWUVFIREExTmIzVnVkR0ZwYmlCV2FXVjNNUk13RVFZRFZRUUtEQXBIYjI5bmJHVWdURXhETVJjdwpGUVlEVlFRRERBNTNkM2N1WjI5dloyeGxMbU52YlRCWk1CTUdCeXFHU000OUFnRUdDQ3FHU000OUF3RUhBMElBCkJORVVBazlkQm5kTHJkci9FV01aWlI2NHZZOXVUdWNkUWM3Ymp4U0lESXFYZCtyVlhRdFg5VzRxZmh5eDhFVHgKZUZ0ZDltLy9QV3M2TDFYS3JlcWdaa0dqZ2dGU01JSUJUakFUQmdOVkhTVUVEREFLQmdnckJnRUZCUWNEQVRBTwpCZ05WSFE4QkFmOEVCQU1DQjRBd0dRWURWUjBSQkJJd0VJSU9kM2QzTG1kdmIyZHNaUzVqYjIwd2FBWUlLd1lCCkJRVUhBUUVFWERCYU1DMEdDQ3NHQVFVRkJ6QUNoaUZvZEhSd09pOHZjR3RwTG1kdmIyY3ZaM055TWk5SFZGTkgKU1VGSE15NWpjblF3S1FZSUt3WUJCUVVITUFHR0hXaDBkSEE2THk5dlkzTndMbkJyYVM1bmIyOW5MMGRVVTBkSgpRVWN6TUIwR0ExVWREZ1FXQkJRZlQxUjFJUVVyc05NVjhFdEd5M2wvcmNPMDF6QU1CZ05WSFJNQkFmOEVBakFBCk1COEdBMVVkSXdRWU1CYUFGSGZDdUZDYVozWjJzUzNDaHRDRG9INm1mcnBMTUNFR0ExVWRJQVFhTUJnd0RBWUsKS3dZQkJBSFdlUUlGQXpBSUJnWm5nUXdCQWdJd01RWURWUjBmQkNvd0tEQW1vQ1NnSW9ZZ2FIUjBjRG92TDJOeQpiQzV3YTJrdVoyOXZaeTlIVkZOSFNVRkhNeTVqY213d0RRWUpLb1pJaHZjTkFRRUxCUUFEZ2dFQkFJWmlXM2pXCm1zOUJ5a0NZUDN1Z2haMDJ2ZE4xZ3dnWmtYa281eVh6TUhyMWtzem5nRFlZLzYrRlB0RHBNb0YwbW4yZ0ZkR1IKK1Z2RUFMaTlLaW95M2s0T0dOaEFtd0NHR2JEelNqYlRIK3dPUHZpdnFPR3lMRUpTbzREeGlqNHZPUU9ENlRUYQpKREhrT0Q3OVFFY3VqRlRjM3lEZzMvZ0M0Tm14dm14SEZ0UlNmenJxSUQ3VG9tTmVyL2NFSE1tTytFRWl6YlR1CjU2L2xiVVpqQ3dkNzB3aFFNZ0wwNWpneXdOWUpVay8waUhZd0JGbjhDRWU1QVlBR3FMeThGYWJDZ2ZSbmFzeW4KTGRSZTRoMU1NaFdvT0toSTdueVNvL3NlS3k5OFRhd0RFczdjcTZwR3ovR1h6RWdYQmVVU1ZEU0lURkM1MFRPVQpEZnFYS3Bpa2Z0MzN5TTg9Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K";
 
     @BeforeEach
-    void setUp() throws IOException, CryptoProviderException {
+    void setUp() throws Exception {
         app = new Application();
         keyPairFile = File.createTempFile("ssl_pinning", ".pem");
         app.generateKeyPair(keyPairFile.getAbsolutePath(), PRIVATE_KEY_PASSWORD);
@@ -44,7 +42,7 @@ class SSLPinningTest {
     }
 
     @Test
-    void testGenerateKeyPairCorrectPassword() throws SSLPinningException {
+    void testGenerateKeyPairCorrectPassword() throws Exception {
         assertNotNull(app.loadPrivateKey(keyPairFile.getAbsolutePath(), PRIVATE_KEY_PASSWORD));
     }
 
@@ -54,7 +52,7 @@ class SSLPinningTest {
     }
 
     @Test
-    void testSignatureWithDetails() throws SSLPinningException, InvalidKeyException, GenericCryptoException, CryptoProviderException {
+    void testSignatureWithDetails() throws Exception {
         CertificateInfo certInfo = app.sign(keyPairFile.getAbsolutePath(), PRIVATE_KEY_PASSWORD,
                 "www.google.com", "9eed43381cf7d58e4563a951364255fc776707a043542a7b997d27c646ee6fb6", 1540280280L);
         byte[] signature = BaseEncoding.base64().decode(certInfo.getSignature());
@@ -65,7 +63,7 @@ class SSLPinningTest {
     }
 
     @Test
-    void testSignatureWithCertInfo() throws SSLPinningException, SignatureException, InvalidKeyException, GenericCryptoException, CryptoProviderException {
+    void testSignatureWithCertInfo() throws Exception {
         CertificateInfo certInfoIn = new CertificateInfo();
         certInfoIn.setName("www.google.com");
         certInfoIn.setExpires(1540280280L);
@@ -79,7 +77,7 @@ class SSLPinningTest {
     }
 
     @Test
-    void testReadCertificate() throws IOException, SSLPinningException {
+    void testReadCertificate() throws Exception {
         File cerFile = File.createTempFile("ssl_pinning", ".cer");
         FileWriter fw = new FileWriter(cerFile.getAbsolutePath());
         fw.write(new String(BaseEncoding.base64().decode(TEST_CERTIFICATE_BASE64)));
@@ -92,7 +90,7 @@ class SSLPinningTest {
     }
 
     @Test
-    void testGenerateJsonFile() throws IOException, SSLPinningException, InvalidKeyException, GenericCryptoException, CryptoProviderException {
+    void testGenerateJsonFile() throws Exception {
         File jsonFile = File.createTempFile("ssl_pinning", ".json");
         File cerFile = File.createTempFile("ssl_pinning", ".cer");
         FileWriter fw = new FileWriter(cerFile.getAbsolutePath());
@@ -107,12 +105,13 @@ class SSLPinningTest {
         scanner.close();
         // replace current signature with static string, it is always different
         generatedJson = generatedJson.replace(certInfo.getSignature(), "SIGNATURE");
-        assertEquals("{\n" +
-                "  \"name\" : \"www.google.com\",\n" +
-                "  \"fingerprint\" : \"nu1DOBz31Y5FY6lRNkJV/HdnB6BDVCp7mX0nxkbub7Y=\",\n" +
-                "  \"expires\" : 1540280280,\n" +
-                "  \"signature\" : \"SIGNATURE\"\n" +
-                "}", generatedJson);
+        assertEquals("""
+                {
+                  "name" : "www.google.com",
+                  "fingerprint" : "nu1DOBz31Y5FY6lRNkJV/HdnB6BDVCp7mX0nxkbub7Y=",
+                  "expires" : 1540280280,
+                  "signature" : "SIGNATURE"
+                }""", generatedJson);
         assertTrue(cerFile.delete());
         assertTrue(jsonFile.delete());
     }
